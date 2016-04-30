4a60c2694369a3cf74933b39326a5796587221b5
Christoph Neumann <christoph@neumannhaus.com>
2016-04-29 17:51:44 -0700

    Fixed the run.sh script so it will "just work".

--- run.sh
+++ run.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
-# Switch to the directory containing the script.
+# Find the directory containing the script.
+base_dir=$(pwd)
 cd $(dirname $(readlink "$0" || echo "$0"))
+code_dir=$(pwd)
 
 # Run the app, bulding it if necessary.
-JAR=$(ls target/scala-2.11/*-assembly-*.jar 2>/dev/null)
+JAR=$(ls "$code_dir"/target/scala-2.11/*-assembly-*.jar 2>/dev/null)
 if [ ! -f "$JAR" ]; then
 	./sbt assembly
+	JAR=$(ls "$code_dir"/target/scala-2.11/*-assembly-*.jar 2>/dev/null)
 fi
+cd "$base_dir"
 java -server -jar "$JAR" $*
